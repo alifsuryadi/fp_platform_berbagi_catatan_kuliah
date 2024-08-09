6b47ed3af6d8d91d45b4cdb5ff6bedebace9571c
faisal1011 <faisalpratama1011@gmail.com>
2024-08-09 16:57:42 +0700

    [FIX] Fix Category Card gap when sm

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Button } from "@nextui-org/react";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold ">Final Project</h1>
-      <Button color="primary">Click Me</Button>
+      <HomePage />
     </>
   );
 }
